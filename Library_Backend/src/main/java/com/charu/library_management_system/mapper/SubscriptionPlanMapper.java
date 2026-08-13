ad99d6082e22863c0e23f5f182800a0f234455f3
CharuBohra <charubohra2004@gmail.com>
2026-08-13 22:13:15 +0530

    Start with Subscription Plan

---
+++ Library_Backend/src/main/java/com/charu/library_management_system/mapper/SubscriptionPlanMapper.java
@@ -0,0 +1,7 @@
+package com.charu.library_management_system.mapper;
+
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface SubscriptionPlanMapper {
+}
---
+++ Library_Backend/src/main/java/com/charu/library_management_system/models/SubscriptionPlan.java
@@ -0,0 +1,13 @@
+package com.charu.library_management_system.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class SubscriptionPlan {
+}
